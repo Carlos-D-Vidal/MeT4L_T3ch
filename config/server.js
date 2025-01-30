51762fb131675faf44cfd6ded4b27a5e085bc974
kyuubinx <kyuubinx@gmail.com>
2025-01-30 11:08:25 -0300

    Exclusão do Express Validator

--- config/server.js
+++ config/server.js
@@ -3,7 +3,6 @@ module.exports = function () {
     const express = require('express')
     const consign = require('consign')
     const bodyParser = require('body-parser')
-    const expressValidator = require('express-validator')
     const expressSession = require('express-session')
     const joi = require('joi')
     
@@ -16,7 +15,6 @@ module.exports = function () {
     app.set('views', './app/views')
 
     app.use(bodyParser.urlencoded({extended: true}))
-    app.use(expressValidator())
     app.use(express.static('app/public'))
 
     app.use(expressSession({
