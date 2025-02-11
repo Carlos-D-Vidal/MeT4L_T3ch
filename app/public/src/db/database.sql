a061fea7bb0372fe3b1f80d45b35d94c8ba1c2bd
Carlos-D-Vidal <ScorpionGamesRS@gmail.com>
2025-02-10 21:36:01 -0300

    Banco atualizado

--- app/public/src/db/database.sql
+++ app/public/src/db/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Fev-2025 às 01:21
+-- Tempo de geração: 11-Fev-2025 às 01:32
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -68,7 +68,8 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`id_cliente`, `nome_cliente`, `natureza`, `cnpj_cpf`, `rg`, `nascimento`, `id_municipio`, `ie`, `bairro`, `numero`, `logradouro`, `email`, `id_promocao`, `status`) VALUES
-(15, 'Lucas', 'PJ', 100255, 1234567890, '2005-05-15', 1, 0, 'Santa Vitória', 38, 'Beco 4 Da Rua Edwino João Haeser', 'Lucas@lucas.com', 1, '1');
+(15, 'Lucas', 'PJ', 100255, 1234567890, '2005-05-15', 1, 0, 'Santa Vitória', 38, 'Beco 4 Da Rua Edwino João Haeser', 'Lucas@lucas.com', 1, '1'),
+(16, 'Teste carlos', 'PJ', 100255, 1234567890, '1999-12-16', 1, 102030405, 'Santa Vitória', 38, 'Beco 4 Da Rua Edwino João Haeser', 'teste@teste.com.br', 2, '1');
 
 -- --------------------------------------------------------
 
@@ -78,24 +79,11 @@ INSERT INTO `cliente` (`id_cliente`, `nome_cliente`, `natureza`, `cnpj_cpf`, `rg
 
 CREATE TABLE `forma_pagamento` (
   `id_forma_pagamento` int(11) NOT NULL,
-  `desc_forma_pagamento` varchar(30) NOT NULL,
+  `descr_forma_pagamento` varchar(30) NOT NULL,
   `id_tipo_recebimento` int(11) NOT NULL,
-  `permite_parcelamento` char(1) NOT NULL,
-  `qtd_max_parcelas` int(11) NOT NULL,
-  `status` char(1) NOT NULL,
-  `codigo` int(11) NOT NULL
+  `qtd_parcelas` int(11) NOT NULL,
+  `status` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Extraindo dados da tabela `forma_pagamento`
---
-
-INSERT INTO `forma_pagamento` (`id_forma_pagamento`, `desc_forma_pagamento`, `id_tipo_recebimento`, `permite_parcelamento`, `qtd_max_parcelas`, `status`, `codigo`) VALUES
-(1, 'Pix', 0, 'N', 1, '', 0),
-(2, 'Credito', 0, 'S', 12, '', 0),
-(3, 'Debito', 0, 'N', 1, '', 0),
-(4, 'Boleto', 0, 'N', 12, '', 0),
-(5, 'Dinheiro', 0, 'N', 1, '', 0);
 
 -- --------------------------------------------------------
 
@@ -114,6 +102,23 @@ CREATE TABLE `item` (
   `registra_comissao` int(11) NOT NULL,
   `status` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Extraindo dados da tabela `item`
+--
+
+INSERT INTO `item` (`id_item`, `desc_item`, `preco_item`, `quant_item`, `codigo`, `codigo_barra`, `id_categoria`, `registra_comissao`, `status`) VALUES
+(6, 'Dark souls', 159.90, 0, 7937, '1', 1, 1, '1'),
+(7, 'Elden ring', 259.90, 0, 1771, '1', 1, 1, '1'),
+(8, 'Amnesia', 39.90, 0, 3695, '1', 2, 1, '1'),
+(9, 'Penumbra', 15.90, 0, 3375, '1', 2, 1, '1'),
+(10, 'Dark souls 2', 69.90, 0, 1041, '9449441594827', 1, 1, '1'),
+(11, 'asdasd', 33.00, 0, 7758, '2199840145484', 2, 1, '1'),
+(12, 'asdasd', 11.00, 0, 3811, '6171776699787', 1, 1, '1'),
+(13, 'adasd', 11.00, 0, 1185, '8455612488015', 1, 1, '1'),
+(14, 'adasd', 11.00, 0, 5562, '7619857252941', 1, 1, '1'),
+(15, 'Game', 30.00, 15, 6551, '7335189492600', 2, 1, '1'),
+(16, 'Dark souls 3', 69.90, 15, 3695, '4438431855268', 1, 1, '1');
 
 -- --------------------------------------------------------
 
@@ -171,7 +176,8 @@ CREATE TABLE `promocao` (
 
 INSERT INTO `promocao` (`id_promocao`, `nome_promocao`, `desconto`) VALUES
 (1, 'Nenhuma', 0),
-(2, 'Natal', 25);
+(2, 'Natal', 25),
+(3, 'Ano novo', 35);
 
 -- --------------------------------------------------------
 
@@ -203,25 +209,24 @@ CREATE TABLE `sys_log_registro` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tipo_operacao`
---
-
-CREATE TABLE `tipo_operacao` (
-  `id_tipo_operacao` int(11) NOT NULL,
-  `denominacao` varchar(30) NOT NULL,
-  `status` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `tipo_recebimento`
 --
 
 CREATE TABLE `tipo_recebimento` (
   `id_tipo_recebimento` int(11) NOT NULL,
-  `desc_tipo_recebimento` varchar(30) NOT NULL
+  `descr_tipo_recebimento` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Extraindo dados da tabela `tipo_recebimento`
+--
+
+INSERT INTO `tipo_recebimento` (`id_tipo_recebimento`, `descr_tipo_recebimento`) VALUES
+(1, 'Dinheiro'),
+(2, 'Pix'),
+(3, 'Credito'),
+(4, 'Debito'),
+(5, 'Boleto');
 
 -- --------------------------------------------------------
 
@@ -305,7 +310,8 @@ ALTER TABLE `cliente`
 -- Índices para tabela `forma_pagamento`
 --
 ALTER TABLE `forma_pagamento`
-  ADD PRIMARY KEY (`id_forma_pagamento`);
+  ADD PRIMARY KEY (`id_forma_pagamento`),
+  ADD KEY `fk_id_tipo_recebimento` (`id_tipo_recebimento`);
 
 --
 -- Índices para tabela `item`
@@ -383,7 +389,7 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `forma_pagamento`
@@ -395,7 +401,7 @@ ALTER TABLE `forma_pagamento`
 -- AUTO_INCREMENT de tabela `item`
 --
 ALTER TABLE `item`
-  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `item_venda`
@@ -413,7 +419,7 @@ ALTER TABLE `municipio`
 -- AUTO_INCREMENT de tabela `promocao`
 --
 ALTER TABLE `promocao`
-  MODIFY `id_promocao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_promocao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `recebimento_venda`
@@ -431,7 +437,7 @@ ALTER TABLE `sys_log_registro`
 -- AUTO_INCREMENT de tabela `tipo_recebimento`
 --
 ALTER TABLE `tipo_recebimento`
-  MODIFY `id_tipo_recebimento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tipo_recebimento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `tipo_usuario`
@@ -461,6 +467,12 @@ ALTER TABLE `venda`
 ALTER TABLE `cliente`
   ADD CONSTRAINT `fk_id_promocao` FOREIGN KEY (`id_promocao`) REFERENCES `promocao` (`id_promocao`),
   ADD CONSTRAINT `fk_municipio` FOREIGN KEY (`id_municipio`) REFERENCES `municipio` (`id_municipio`);
+
+--
+-- Limitadores para a tabela `forma_pagamento`
+--
+ALTER TABLE `forma_pagamento`
+  ADD CONSTRAINT `fk_id_tipo_recebimento` FOREIGN KEY (`id_tipo_recebimento`) REFERENCES `tipo_recebimento` (`id_tipo_recebimento`);
 
 --
 -- Limitadores para a tabela `item`
